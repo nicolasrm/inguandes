89a785fe5a355d4c7a8fdb2b5166fcea9bb838f1
Matías Recabarren <mat.reca@gmail.com>
2012-08-04 22:16:10 -0400

    ref #43 ver resultados generales
    Se muestran resultados resumen de los alumnos.

--- web2py/applications/inguandes/controllers/quiz.py
+++ web2py/applications/inguandes/controllers/quiz.py
@@ -51,11 +51,15 @@ def result():
     quizId = int(request.args[0])
     quiz_info = get_quiz(db, quizId)
     
+    user_role = get_user_role(db, quiz_info['instance_id'], auth.user.id)
+    if user_roles[user_role] == 'Profesor' or user_roles[user_role] == 'Ayudante Jefe':
+        redirect(URL('quiz', 'all_result', args=[quizId]))
+    
     if quiz_info['ending'] >= datetime.datetime.now():
         redirect(URL('instance', 'view', args=[quiz_info['instance_id']]))
         
     user_quiz = get_user_quiz(db, quizId, auth.user.id)
-    user_resullt = quiz_user_result(db, user_quiz)
+    user_resullt = quiz_user_result(db, quiz_info, user_quiz)
     
     return dict(quiz_info=quiz_info, user_quiz=user_quiz, user_resullt=user_resullt)
     
--- web2py/applications/inguandes/models/sections_util.py
+++ web2py/applications/inguandes/models/sections_util.py
@@ -14,8 +14,9 @@ def get_sections(db, courseid=None):
                         term = s.section.term,
                         term_name = s.term.name
                         )
-        section['students'] = db((db.user_section.section==s.section.id) & (db.user_section.the_role==1)).count()
-        section['assistants'] = db((db.user_section.section==s.section.id) & (db.user_section.the_role==2)).count()
+        section['students'] = db((db.user_section.section==s.section.id) & (db.user_section.the_role==0)).count()
+        section['assistants'] = db((db.user_section.section==s.section.id) & (db.user_section.the_role==1)).count()
+        section['assistants_boss'] = db((db.user_section.section==s.section.id) & (db.user_section.the_role==2)).count()
         section['professors'] = db((db.user_section.section==s.section.id) & (db.user_section.the_role==3)).count()
         sections_a.append(section)
     return sections_a
