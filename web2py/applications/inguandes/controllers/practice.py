bfe02f510696f8963ec056071e202365725f4c1a
Matias Recabarrem <mrecabarren@miuandes.cl>
2012-10-10 18:11:15 -0300

    ref #61 formularios basicos
    Se registra la información principal de solicitud.

--- web2py/applications/inguandes/controllers/practice.py
+++ web2py/applications/inguandes/controllers/practice.py
@@ -15,4 +15,59 @@ def add_practice():
                                     category=request.vars.category)
     
     redirect(URL('view', args=[p_id]))
+    
+def update_company():
+    p_id = request.vars.practiceid
+    practice = db.practice[p_id]
+    
+    if practice.company is None:
+        com_id = db.company.insert( rut=request.vars.rut,
+                                    name=request.vars.name,
+                                    businessLine=request.vars.businessLine,
+                                    address=request.vars.address,
+                                    city=request.vars.city,
+                                    country=request.vars.country)
+        practice.update_record(company=com_id)
+    else:
+        com = db.company[practice.company]
+        com.update_record(  rut=request.vars.rut,
+                            name=request.vars.name,
+                            businessLine=request.vars.businessLine,
+                            address=request.vars.address,
+                            city=request.vars.city,
+                            country=request.vars.country)
+        
+    redirect(URL('view', args=[p_id]))
+    
+def update_validator():
+    p_id = request.vars.practiceid
+    practice = db.practice[p_id]
+    if practice.validator is None:
+        emp_id = db.company_employee.insert(company=practice.company,
+                                            first_name=request.vars.first_name,
+                                            last_name=request.vars.last_name,
+                                            position=request.vars.position,
+                                            department=request.vars.department,
+                                            phone=request.vars.phone,
+                                            email=request.vars.email)
+        practice.update_record(validator=emp_id)
+    else:
+        emp = db.company_employee[practice.validator]
+        emp.update_record(  company=practice.company,
+                            first_name=request.vars.first_name,
+                            last_name=request.vars.last_name,
+                            position=request.vars.position,
+                            department=request.vars.department,
+                            phone=request.vars.phone,
+                            email=request.vars.email)
+    
+    redirect(URL('view', args=[p_id]))
 
+def update_practice():
+    p_id = request.vars.practiceid
+    practice = db.practice[p_id]
+    practice.update_record( description=request.vars.description,
+                            starting=request.vars.starting,
+                            ending=request.vars.ending)
+    
+    redirect(URL('view', args=[p_id]))
--- web2py/applications/inguandes/models/practice_util.py
+++ web2py/applications/inguandes/models/practice_util.py
@@ -14,6 +14,46 @@ def get_practice(db, pId):
     p_info['user'] = p.the_user
     p_info['category'] = p.category
     p_info['category_name'] = practice_category[p.category]
+    p_info['company'] = get_company(db, p.company)
+    p_info['validator'] = get_employee(db, p.validator)
     p_info['state'] = 'pending'
+    p_info['description'] = p.description
+    p_info['starting'] = p.starting
+    p_info['ending'] = p.ending
     
     return p_info 
+
+def get_company(db, comId):
+    com = db(db.company.id == comId).select(db.company.ALL).first()
+    
+    if com is not None:
+        com_info = {}
+        com_info['id'] = com.id
+        com_info['rut'] = com.rut
+        com_info['name'] = com.name
+        com_info['businessLine'] = com.businessLine
+        com_info['address'] = com.address
+        com_info['city'] = com.city
+        com_info['country'] = com.country
+        
+        return com_info
+    else:
+        return None
+        
+def get_employee(db, empId):
+    emp = db(db.company_employee.id == empId).select(db.company_employee.ALL).first()
+    
+    if emp is not None:
+        emp_info = {}
+        emp_info['id'] = emp.id
+        emp_info['company'] = get_company(db, emp.company)
+        emp_info['first_name'] = emp.first_name
+        emp_info['last_name'] = emp.last_name
+        emp_info['position'] = emp.position
+        emp_info['department'] = emp.department
+        emp_info['phone'] = emp.phone
+        emp_info['email'] = emp.email
+        
+        return emp_info
+    else:
+        return None
