fded40b59b0e0b2c6b89d64203f483ac8b34b033
Matías Recabarren <mat.reca@gmail.com>
2012-10-10 01:05:04 -0300

    ref #61 inscripcion basica de practica
    Comienzo de creacion de formularios

--- web2py/applications/inguandes/controllers/practice.py
+++ web2py/applications/inguandes/controllers/practice.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 @auth.requires_login()
 def view():
-        
-    return dict()   
+    p_info = None
+    if len(request.args) > 0:
+        p_info = get_practice(db, int(request.args[0]))
+    practices = get_user_practices(db, auth.user.id)
+    return dict(practices=practices, p_info=p_info)   
+
+@auth.requires_login()
+def add_practice():
+    p_id = None
+    if request.vars.category is not None:
+        p_id = db.practice.insert(  the_user=auth.user.id,
+                                    category=request.vars.category)
+    
+    redirect(URL('view', args=[p_id]))
+
---
+++ web2py/applications/inguandes/models/practice_util.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def get_user_practices(db, userId):
+    ps = db(db.practice.the_user==userId).select(db.practice.ALL)
+    
+    ps_info = [get_practice(db, p.id) for p in ps]    
+    return ps_info 
+    
+def get_practice(db, pId):
+    p = db(db.practice.id==pId).select(db.practice.ALL).first()
+        
+    p_info = {}
+    p_info['id'] = p.id
+    p_info['user'] = p.the_user
+    p_info['category'] = p.category
+    p_info['category_name'] = practice_category[p.category]
+    p_info['state'] = 'pending'
+    
+    return p_info 
